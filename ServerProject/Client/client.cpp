342f336959c8c146e3ae790829b7f85b18ca003d
snail196 <1964353196@qq.com>
2018-12-25 11:50:38 +0800

    Add files via upload
    网络报文的数据定义以及使用

--- ServerProject/Client/client.cpp
+++ ServerProject/Client/client.cpp
@@ -1,17 +1,46 @@
 #define WIN32_LEAN_AND_MEAN
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define  _CRT_SECURE_NO_WARNINGS
+
 #include<windows.h>
 #include<WinSock2.h>
 #include<stdio.h>
 
 #pragma comment(lib,"ws2_32.lib")
 
-struct DataPackage
+enum CMD
 {
-	int age;
-	char name[32];
+	CMD_LOGIN,
+	CMD_LOGOUT,
+	CMD_ERROR
 };
+struct DataHeader
+{
+	short dataLength;
+	short cmd;
+};
+//DataPackage
+struct Login
+{
+	char userName[32];
+	char PassWord[32];
+};
+
+struct LoginResult
+{
+	int result;
+
+};
+
+struct Logout
+{
+	char userName[32];
+};
+
+struct LogoutResult
+{
+	int result;
+
+};
+
 
 int main()
 {
@@ -53,20 +82,36 @@ int main()
 		//4 处理请求命令
 		if (0 == strcmp(cmdBuf, "exit"))
 		{
-			printf("收到exit命令，任务结束。");
+			printf("收到exit命令，任务结束。\n");
 			break;
-		}else{
+		}else if (0 == strcmp(cmdBuf, "login")) {
+			Login login = {"lyd","lydmm"};
+			DataHeader dh = { sizeof(login),CMD_LOGIN};
 			//5 向服务器发送请求命令
-			send(_sock, cmdBuf, strlen(cmdBuf) + 1, 0);
+			send(_sock, (const char *)&dh, sizeof(dh), 0);
+			send(_sock, (const char *)&login, sizeof(login), 0);
+			// 接收服务器返回的数据
+			DataHeader retHeader = {};
+			LoginResult loginRet = {};
+			recv(_sock, (char*)&retHeader, sizeof(retHeader), 0);
+			recv(_sock, (char*)&loginRet, sizeof(loginRet), 0);
+			printf("LoginResult: %d \n", loginRet.result);
 		}
-
-		// 6 接收服务器信息 recv
-		char recvBuf[128] = {};
-		int nlen = recv(_sock, recvBuf, 128, 0);
-		if (nlen > 0)
-		{
-			DataPackage* dp = (DataPackage*)recvBuf;
-			printf("接收到数据：年龄=%d ，姓名=%s\n", dp->age, dp->name);
+		else if (0 == strcmp(cmdBuf, "logout")) {
+			Logout logout = { "lyd" };
+			DataHeader dh = { sizeof(logout), CMD_LOGOUT };
+			//5 向服务器发送请求命令
+			send(_sock, (const char *)&dh, sizeof(dh), 0);
+			send(_sock, (const char *)&logout, sizeof(logout), 0);
+			// 接收服务器返回的数据
+			DataHeader retHeader = {};
+			LogoutResult logoutRet = {};
+			recv(_sock, (char*)&retHeader, sizeof(retHeader), 0);
+			recv(_sock, (char*)&logoutRet, sizeof(logoutRet), 0);
+			printf("LogoutResult: %d \n", logoutRet.result);
+		}
+		else {
+			printf("不支持的命令，请重新输入。\n");
 		}
 	}
 	// 7 关闭套节字closesocket
